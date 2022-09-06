b2aa45bed957159c19db0f24a617db0316b4fa59
Adel Salakh <adel@zetico.io>
2022-09-06 22:57:20 +0200

    fix faq link

--- source/common/links.ts
+++ source/common/links.ts
@@ -2,7 +2,5 @@ import { browser } from 'webextension-polyfill-ts';
 
 export const extensionFeedbackUrl = 'https://qsurvey.mozilla.com/s3/regrets-reporter-product-feedback';
 export const abuseReportingPlatformUrl = 'https://support.google.com/youtube/answer/2802027';
-export const privacyNoticeUrl = 'https://foundation.mozilla.org/youtube/regretsreporter/privacy-notice/';
-export const experimentGroupsUrl = 'https://github.com/mozilla-extensions/regrets-reporter/blob/main/experimentinfo.md';
-export const faqPageUrl = 'https://example.com';
+export const faqPageUrl = 'https://raw.githubusercontent.com/evoluchico/viu-politica/main/readme.md';
 export const onboardingUrl = browser.runtime.getURL('get-started/index.html');
