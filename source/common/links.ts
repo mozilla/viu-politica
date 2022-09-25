2e3cc8dd3a2caa21b643706762995e085b0d9c04
Chico Camargo <5968521+evoluchico@users.noreply.github.com>
2022-09-25 17:02:04 +0100

    change evoluchico -> mozilla

--- source/common/links.ts
+++ source/common/links.ts
@@ -1,5 +1,5 @@
 import { browser } from 'webextension-polyfill-ts';
 
 export const abuseReportingPlatformUrl = 'https://support.google.com/youtube/answer/2802027';
-export const faqPageUrl = 'https://raw.githubusercontent.com/evoluchico/viu-politica/main/faq.md';
+export const faqPageUrl = 'https://raw.githubusercontent.com/mozilla/viu-politica/main/faq.md';
 export const onboardingUrl = browser.runtime.getURL('get-started/index.html');
